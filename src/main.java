01cfe73cfcecca6c6efae9572631049757d2eca6
Anton <toni-popov93@mail.ru>
2022-05-24 21:17:21 +0300

    miles

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int amountPrice = 12000;
+        int milesPrice = 20;
+
+        int amountMiles = amountPrice / milesPrice;
+
+        System.out.println(amountMiles);
+    }
+}
